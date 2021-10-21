53c8080a41164715cefb197716d030bbb601b7e6
Roberto Paz <fsjedi@outlook.com>
2021-10-21 10:38:53 -0400

    genera aleatorio

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+let humanScore = 0
+let computerScore = 0
+let currentRoundNumber = 1
 
 // Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 10)
 
+console.log(generateTarget())
+
+generateTarget()
+
+/*
+Create a generateTarget() function. 
+This function will be called at the start of each new
+round in order to generate the new secret target number.
+
+This function should return a random integer between 0 and 9.
+*/
